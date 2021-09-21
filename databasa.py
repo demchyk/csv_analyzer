b3470a89b5b03e508cd80ca5cba9ffde74347c58
denek98 <devimov1@gmail.com>
2021-09-21 22:21:13 +0300

    Database class created

---
+++ databasa.py
@@ -0,0 +1,65 @@
+import sqlite3
+import pandas as pd
+from metric_counters import *
+
+class DataBasa:
+
+
+	def __init__(self,zte_object,db_name,table_name):
+		self.__headers = zte_object.headers
+		self.__metrics = zte_object.metrics
+		self.__primary_keys = zte_object.primary_keys
+		self.__counters = zte_object.counters
+		self.__db_name = str(db_name)+'.db'
+		self.__table_name = table_name
+		self.__connection, self.__cursor = self.__class__.__init_db_connection(self.__db_name)
+
+
+	@staticmethod
+	def __init_db_connection(db_name):
+		return sqlite3.connect(db_name),sqlite3.connect(db_name).cursor()
+
+
+
+	def fill_table(self,file):
+		if not self.__check_csv(file,self.__primary_keys,self.__counters):
+			print('Sorry')
+		else:
+			try:
+				self.__cursor.execute(f''' CREATE TABLE IF NOT EXISTS {self.__table_name + '_temp'} ({self.__headers},primary key ({",".join(self.__primary_keys)})) ''')
+				useful_values_from_csv = self.__get_useful_values_from_csv(file,self.__counters) + self.__primary_keys
+				data_frame_from_csv = pd.read_csv(file, usecols = useful_values_from_csv)
+				data_frame_from_csv.to_sql(self.__table_name + '_temp', self.__connection, if_exists = 'append',index = False)
+				self.__connection.commit()
+			except sqlite3.IntegrityError:
+				print('Повторяемся') #need to skip
+			finally:
+				self.__connection.close()
+
+	@staticmethod
+	def __check_csv(file,primary_keys,counters):
+		file_header_values = pd.read_csv(file, nrows = 0).columns.tolist()
+		if not all(key in file_header_values for key in primary_keys):
+			return False #Не все ключи присутствуют, скипаем
+		else:
+			for value in file_header_values:
+				if value in counters:
+					return True
+			return False
+
+	@staticmethod
+	def __get_useful_values_from_csv(file,counters):
+		useful_values = []
+		file_header_values = pd.read_csv(file, nrows = 0).columns.tolist()
+		for value in file_header_values:
+			if value in counters:
+				useful_values.append(value)
+		return useful_values
+
+
+file = 'formula.txt'
+keys = 'GSM_PKEY.txt'
+zek = 'test.csv'
+GSM = ZTE_Object(file,keys)
+db = DataBasa(GSM,'basa','table')
+db.fill_table(zek)
---
+++ metric_counters.py
@@ -0,0 +1,59 @@
+class ZTE_Object:
+
+	
+	def __init__(self,instructions_file,primary_keys_file):
+		self.__counters = self.__class__.__extract_counters_from_formulas(instructions_file)
+		self.__metrics = self.__class__.__extract_metrics_from_file(instructions_file)
+		self.__primary_keys = self.__class__.__extract_primary_keys_from_file(primary_keys_file)
+		self.__headers = ','.join(self.__primary_keys + self.__counters)
+
+	@staticmethod
+	def __extract_formulas_from_file(instructions_file):
+		with open(instructions_file,'rt') as f:
+			formulas = [formulas_temp.strip() for formulas_temp in f.readlines() if not formulas_temp.isspace()]
+		return formulas
+
+	@staticmethod
+	def __extract_primary_keys_from_file(primary_keys_file):
+		with open(primary_keys_file,'rt') as f:
+			primary_keys = [keys_temp.strip() for keys_temp in f.readlines() if not keys_temp.isspace()]
+		return primary_keys
+
+	@classmethod
+	def __extract_metrics_from_file(cls,instructions_file):
+		metrica_dic = {}
+		formulas = cls.__extract_formulas_from_file(instructions_file)
+		for formula in formulas:
+			metrica = formula[:formula.find('=')].strip()
+			expression = formula[formula.find('=') + 1:]
+			metrica_dic[metrica] = expression.strip()
+		return metrica_dic
+
+	@classmethod
+	def __extract_counters_from_formulas(cls,instructions_file):
+		formulas = cls.__extract_formulas_from_file(instructions_file)
+		all_counters = []
+		for formula in formulas:
+			expression = formula[formula.find('=') + 1:]
+			bad_words = '+-/*:.,()'
+			for word in bad_words:
+				expression = expression.replace(word,' ')
+			text_list = [word.strip() for word in expression.split()]
+		all_counters.append(text_list[1:])
+		return list(set(all_counters[0]))
+
+	@property
+	def counters(self):
+		return self.__counters
+	@property
+	def metrics(self):
+		return self.__metrics
+	@property
+	def primary_keys(self):
+		return self.__primary_keys
+	@property
+	def headers(self):
+		return self.__headers
+
+
+ 			
