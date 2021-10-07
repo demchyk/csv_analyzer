99c76382aecbc6d608358d3463a8eb610144728c
demchyk <nataliiademchyk@gmail.com>
2021-10-07 16:39:22 +0300

    add zip chunking by size

--- unpacking_folders.py
+++ unpacking_folders.py
@@ -2,6 +2,7 @@ import os
 import zipfile
 import shutil
 
+
 class Net_Folder:
     def __new__(cls, path):
         if cls.__check_requirements(path) and cls.__check_input_data(path):
@@ -12,7 +13,7 @@ class Net_Folder:
     def __init__(self, path):
         self.__path = path
         # self.__class__.__processing_input(self.__path)
-        self.__zipfiles_list = self.__class__.__get_zipfile_list(self.__path)
+        self.__zipfiles_list = self.__class__.__get_ziplist_by_size(self.__path)
 
     @property
     def csv_path(self):
@@ -45,32 +46,53 @@ class Net_Folder:
 
     @staticmethod
     def __check_input_data(path):
-        cur_path = path + "/input_data/"
-        if os.listdir(cur_path):
+        if os.listdir(path + "/input_data/"):
             return True
         else:
             return False
 
     @classmethod
     def __processing_input(cls, path):
-        cur_path = path + "/input_data"
-        for address, dirs, files in os.walk(cur_path):
+        for address, dirs, files in os.walk(path + "/input_data/"):
             for file in files:
                 if file.endswith(".zip"):
                     zip_ref = zipfile.ZipFile(os.path.join(address, file))
                     zip_ref.extractall(path + "/processed_files/")
                     zip_ref.close()
-        # cls.__remove_directory_content(cur_path)
+        # cls.__remove_directory_content(path + "/input_data/")
 
     @classmethod
-    def __get_zipfile_list(cls, path):
-        li = [f.path for f in os.scandir(path + "/input_data") if f.is_dir()]
+    def __get_ziplist_by_folder(cls, path):
+        li = [f.path for f in os.scandir(
+            path + "/input_data") if f.is_dir() and os.listdir(f)]
         rez = [[] for item in li]
         for i in range(len(li)):
             for address, dirs, files in os.walk(li[i]):
                 for file in files:
                     if file.endswith(".zip"):
                         rez[i].append(os.path.join(address, file))
+        return rez
+
+    @classmethod
+    def __get_ziplist_by_size(cls, path):
+        temp_li = []
+        for address, dirs, files in os.walk(path + "/input_data"):
+            for file in files:
+                if file.endswith(".zip"):
+                    temp_li.append([os.path.join(address, file), os.path.getsize(os.path.join(address, file))])
+        temp_li.sort(key=lambda x: x[0])
+        rez = [[]]
+        i = 0
+        size_counter = 0
+        for item in temp_li:
+            size_counter += item[1]
+            if size_counter >= (1.5 * 10**9):
+                rez.append([])
+                i += 1
+                rez[i].append(item[0])
+                size_counter = 0
+            else:
+                rez[i].append(item[0])
         return rez
 
     @staticmethod
@@ -84,7 +106,6 @@ class Net_Folder:
 
 
 # time1 = time.time()
-# lte = Net_Folder(os.path.dirname(os.path.realpath('__file__')) + "/ZTE/WCDMA")
+lte = Net_Folder(os.path.dirname(os.path.realpath('__file__')) + "/ZTE/WCDMA")
 # time2 = time.time()
 # print(time2 - time1)
-
