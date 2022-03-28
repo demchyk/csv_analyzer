77d0794e9b3a15c66db5a61cf7f59808962ca1ca
denek98 <defimov1@gmail.com>
2022-03-28 15:23:20 +0300

    commit

--- application/engine.py
+++ application/engine.py
@@ -3,14 +3,14 @@ from .metric_counters import *
 from .databasa import *
 from .agg import *
 
+
 def start_filling(zte_type):
-	cur_folder = Net_Folder(os.path.dirname(os.path.realpath('__file__')) + "/ZTE/" + zte_type) # Getting all static files for selected ZTE type
-	ZTE = ZTE_Object(cur_folder) # Calculating counters, metrics and primary keys for selected ZTE type
-	db = DataBasa(ZTE,zte_type) 
-	db.result_to_pickle() # Creating pickle with attributes for selected ZTE type
-
-def start_agg_to_csv(time_interval,claster_check,aggregation_time_type,aggregation_type):
-	aggr = Aggregation(time_interval,claster_check,aggregation_time_type,aggregation_type) # setting form request values as arguments
-	aggr.aggregate_to_csv()
+    cur_folder = Net_Folder(os.path.dirname(os.path.realpath('__file__')) + "/ZTE/" + zte_type)  # Getting all static files for selected ZTE type
+    ZTE = ZTE_Object(cur_folder)  # Calculating counters, metrics and primary keys for selected ZTE type
+    db = DataBasa(ZTE, zte_type)
+    db.result_to_pickle()  # Creating pickle with attributes for selected ZTE type
 
 
+def start_agg_to_csv(time_interval, claster_check, aggregation_time_type, aggregation_type):
+    aggr = Aggregation(time_interval, claster_check, aggregation_time_type, aggregation_type)  # setting form request values as arguments
+    aggr.aggregate_to_csv()
